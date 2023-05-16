8ea1b320986ec755e41576042f1c0c4ba095258e
Nandana <nandana@conscala.com>
2023-05-16 12:54:00 +0530

    Added iOS Target

---
+++ iOSSolvesUtilities IOS/iOSSolvesUtilities_iOS.h
@@ -0,0 +1,18 @@
+//
+//  iOSSolvesUtilities_iOS.h
+//  iOSSolvesUtilities iOS
+//
+//  Created by Nandana Bandikatla on 16/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSSolvesUtilities_iOS.
+FOUNDATION_EXPORT double iOSSolvesUtilities_iOSVersionNumber;
+
+//! Project version string for iOSSolvesUtilities_iOS.
+FOUNDATION_EXPORT const unsigned char iOSSolvesUtilities_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSSolvesUtilities_iOS/PublicHeader.h>
+
+
